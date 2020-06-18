c6e1077c9dc78ddfdafc0f2bc42c0a5f239717cb
hiddenhelper <bountyhunter0308@hotmail.com>
2020-06-17 23:32:48 -0700

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,43 @@ import { Link } from "react-router-dom";
 
 const Navbar = (pageType) => {
   var navType = "navHome";
-  if ((pageType = "About")) navType = "navAbout";
-  else if ((pageType = "Book")) navType = "navBook";
-  else if ((pageType = "Contact")) navType = "navContact";
+  if ((pageType = "About")) {
+    navType = "navAbout";
+    return (
+      <nav className={navType}>
+        <section>
+          <Link to="/About">ABOUT</Link>
+          <Link to="/Book">THE BOOK</Link>
+          <Link to="/Contact">CONTACT</Link>
+        </section>
+      </nav>
+    );
+  } else if ((pageType = "Book")) {
+    navType = "navBook";
+    return (
+      <nav className={navType}>
+        <section>
+          <Link to="/About">ABOUT</Link>
+          <Link to="/Book">THE BOOK</Link>
+          <Link to="/Contact">CONTACT</Link>
+        </section>
+      </nav>
+    );
+  } else if ((pageType = "Contact")) {
+    navType = "navContact";
+    return (
+      <nav className={navType}>
+        <section>
+          <Link to="/About">ABOUT</Link>
+          <Link to="/Book">THE BOOK</Link>
+          <Link to="/Contact">CONTACT</Link>
+        </section>
+      </nav>
+    );
+  }
 
   return (
-    <nav>
+    <nav className={navType}>
       <section>
         <Link to="/About">ABOUT</Link>
         <Link to="/Book">THE BOOK</Link>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 class HomePage extends Component {
   render() {
-    const pageType = "About";
+    const pageType = "Home";
     return (
       <Fragment>
         <Navbar pageType={pageType} />
